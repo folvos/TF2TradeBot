7064f7d77393985e2a7e8f9e2095ea501e09c5d3
Aidan <yayes.aidan@gmail.com>
2016-09-24 12:39:21 -0400

    Staging for automatic tradeoffers

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const Steam = require('steam')
+const getSteamAPIKey = require('steam-web-api-key')
+const SteamWebLogOn = require('steam-weblogon')
+const SteamTradeOffers = require('steam-tradeoffers')
--- src/mobileauth.js
+++ src/mobileauth.js
@@ -1,51 +1,68 @@
-const crypto = require('crypto')
+// const crypto = require('crypto')
 const path = require('path')
 const fs = require('fs')
+const steamTOTP = require('steam-totp')
 
-function initSecrets (pass) {
-  let manifest = JSON.parse(fs.readFileSync(path.join(__dirname, '../', 'mobile_auth', 'manifest.json')))
-  let maFile = fs.readFileSync(path.join(__dirname, '../', 'mobile_auth', manifest.entries[0].filename))
-  let secrets = {}
+class MobileAuthHandler {
+  var sharedSecret = ''
+  var identitySecret = ''
 
-  if (pass === '' || pass === undefined) {
-    maFile = JSON.parse(maFile)
-    secrets = {
-      sharedSecret: maFile.shared_secret,
-      identitySecret: maFile.identity_secret
-    }
-
-    console.log('Loaded Steam Desktop Authenticator data.')
-  } else {
-    console.log('Decryption is currently not supported.')
-    process.exit(1)
-
-    /*
-    TODO: Fix decryption.
-    maFile = decryptMaFile(pass, maFile, manifest)
-    secrets = {
-      sharedSecret: maFile.shared_secret,
-      identitySecret: maFile.identity_secret
-    }
-    */
+  MobileAuthHandler(sharedSecret, identitySecret) {
+    if (sharedSecret == null || identitySecret == null)
+    this.sharedSecret = sharedSecret
+    this.identitySecret = identitySecret
   }
 
-  return secrets
+  initSecrets (pass) {
+    let manifest = JSON.parse(fs.readFileSync(path.join(__dirname, '../', 'mobile_auth', 'manifest.json')))
+    let maFile = fs.readFileSync(path.join(__dirname, '../', 'mobile_auth', manifest.entries[ 0 ].filename))
+    let secrets = {}
+
+    if (pass === '' || pass === undefined) {
+      maFile = JSON.parse(maFile)
+      secrets = {
+        sharedSecret: maFile.shared_secret,
+        identitySecret: maFile.identity_secret
+      }
+
+      console.log('Loaded Steam Desktop Authenticator data.')
+    } else {
+      console.log('Decryption is currently not supported.')
+      process.exit(1)
+
+      /*
+       TODO: Fix decryption.
+       maFile = decryptMaFile(pass, maFile, manifest)
+       secrets = {
+       sharedSecret: maFile.shared_secret,
+       identitySecret: maFile.identity_secret
+       }
+       */
+    }
+
+    return secrets
+  }
+
+  /*
+  TODO: Fix decryption.
+  decryptMaFile (pass, maFile, manifest) {
+   let salt = Buffer.from(manifest.entries[0].encryption_salt, 'base64').toString('binary')
+   let key = crypto.pbkdf2Sync(pass, salt, 50000, 32, 'sha256')
+
+   maFile = Buffer.from(maFile, 'base64').toString('binary')
+   let decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(manifest.entries[0].encryption_iv, 'base64'))
+   decipher.setAutoPadding(true)
+   let decoded = decipher.update(maFile, 'binary', 'utf8')
+
+   decoded += decipher.final('utf8')
+   return decoded
+   }
+   */
+
+
+  getTOTPToken() {
+    steamTOTP.generateAuthCode(this.sharedSecret)
+  }
 }
-
-/*
-TODO: Fix decryption.
-function decryptMaFile (pass, maFile, manifest) {
-  let salt = Buffer.from(manifest.entries[0].encryption_salt, 'base64').toString('binary')
-  let key = crypto.pbkdf2Sync(pass, salt, 50000, 32, 'sha256')
-
-  maFile = Buffer.from(maFile, 'base64').toString('binary')
-  let decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(manifest.entries[0].encryption_iv, 'base64'))
-  decipher.setAutoPadding(true)
-  let decoded = decipher.update(maFile, 'binary', 'utf8')
-
-  decoded += decipher.final('utf8')
-  return decoded
-}
-*/
 
 console.log(initSecrets())
