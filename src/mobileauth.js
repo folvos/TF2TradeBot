75c2e966b9729accd76e83902db0eecf3bc66c3b
JasLeMarsh <jaslemarsh@gmail.com>
2016-09-24 13:30:31 -0400

    Work on fixing decryption

--- src/mobileauth.js
+++ src/mobileauth.js
@@ -1,4 +1,5 @@
-// const crypto = require('crypto')
+const aesjs = require('aes-js')
+const crypto = require('crypto')
 const path = require('path')
 const fs = require('fs')
 const steamTOTP = require('steam-totp')
@@ -43,22 +44,28 @@ class MobileAuthHandler {
     return secrets
   }
 
-  /*
-  TODO: Fix decryption.
+
+  //TODO: Fix decryption.
   decryptMaFile (pass, maFile, manifest) {
-   let salt = Buffer.from(manifest.entries[0].encryption_salt, 'base64').toString('binary')
-   let key = crypto.pbkdf2Sync(pass, salt, 50000, 32, 'sha256')
+    let salt = Buffer.from(manifest.entries[0].encryption_salt, 'base64').toString('binary')
+    let key = crypto.pbkdf2Sync(pass, salt, 50000, 32, 'sha256')
 
-   maFile = Buffer.from(maFile, 'base64').toString('binary')
-   let decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(manifest.entries[0].encryption_iv, 'base64'))
-   decipher.setAutoPadding(true)
-   let decoded = decipher.update(maFile, 'binary', 'utf8')
+    maFile = Buffer.from(maFile, 'base64')
+    /*let decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(manifest.entries[0].encryption_iv, 'base64'))
+    decipher.setAutoPadding(true)
+    let decoded = decipher.update(maFile, 'binary', 'utf8')
 
-   decoded += decipher.final('utf8')
-   return decoded
-   }
-   */
+    decoded += decipher.final('utf8')
+    return decoded*/
+    // The cipher-block chaining mode of operation maintains internal
+    // state, so to decrypt a new instance must be instantiated.
+    const aesCbc = new aesjs.ModeOfOperation.cbc(key, Buffer.from(manifest.entries[0].encryption_iv, 'base64'))
+    const decryptedBytes = aesCbc.decrypt(maFile)
 
+    // Convert our bytes back into text
+    const decryptedText = aesjs.util.convertBytesToString(decryptedBytes)
+    return JSON.parse(decryptedText)
+  }
 
   getTOTPToken() {
     steamTOTP.generateAuthCode(this.sharedSecret)
